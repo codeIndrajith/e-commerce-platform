380a84bc6fe102f0d1ed4d5a51741ae5bc455515
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-02-19 13:44:40 +0530

    connect with mongoose
    connect the database with mongoose

--- back-end/server.js
+++ back-end/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.get('/', (req, res, next) => {
   res.send('API is running..');
