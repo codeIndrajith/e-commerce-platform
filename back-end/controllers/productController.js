8a4e675d6ca56c2f7a94fd78cd5406ae312fed1b
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-02-22 21:46:24 +0530

    Product controllers customize

--- back-end/controllers/productController.js
+++ back-end/controllers/productController.js
@@ -1,12 +1,17 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import Product from '../model/productModel.js';
 
+// @desc      Fetch all products
+// @route     GET/api/products
+// @access    Public
 const getAllProducts = asyncHandler(async (req, res, next) => {
   const products = await Product.find({});
-  throw new Error('Some Error');
   res.json(products);
 });
 
+//  @desc     Fetch the specific product (using id)
+// @route     GET/api/products/:id
+// @access    Public
 const getSpecificProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
 
--- back-end/routes/productRoutes.js
+++ back-end/routes/productRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import productsControllers from '../controllers/productController.js';
 const router = express.Router();
 
-router.get('/', productsControllers.getAllProducts);
+router.get('/', productsControllers.getAllProducts); // or we can write this code        router.route('/').get(productsControllers.getAllProducts) -> route identified in what is use request method like get,post.. using endpoint
 
 router.get('/:id', productsControllers.getSpecificProduct);
 
