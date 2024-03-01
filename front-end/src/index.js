f6588764b2ead316c8906e769741c845b51354da
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-03-01 09:49:02 +0530

    Redux store & API Slice create
    This part we use state management, so we install redux-toolkit and react-redux packages.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,10 +6,12 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import store from './store';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -23,6 +25,8 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
---
+++ front-end/src/slices/apiSlices..js
@@ -0,0 +1,12 @@
+// Remember the api slices is a parent of other apis
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'; // fetchBaseQuery allow to make request to api
+import { BASE_URL } from '../constants.js';
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ['Product', 'User', 'Order'], // <- the tagTypes is a what are these fetch from back end
+  endpoints: (builder) => ({}),
+});
