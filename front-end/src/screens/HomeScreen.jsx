80dfeb62e02ee65e0a7e44c03f7fb5e22e5382c5
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-03-01 12:27:15 +0530

    All products fetch from redux
    Products API Slice & Get Products Endpoint.

--- front-end/src/screens/HomeScreen.jsx
+++ front-end/src/screens/HomeScreen.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
 import Product from '../components/Product';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []);
   return (
     <>
-      <h1 className="text-2xl font-bold mb-4">Latest Products</h1>
-      <div className="grid grid-cols-12 gap-4">
-        {products.map((product) => (
-          <div
-            key={product._id}
-            className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3"
-          >
-            <div className="bg-white p-4 rounded-lg shadow w-full h-full">
-              <Product product={product} />
-            </div>
+      {isLoading ? (
+        <h1>isLoading......</h1>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1 className="text-2xl font-bold mb-4">Latest Products</h1>
+          <div className="grid grid-cols-12 gap-4">
+            {products.map((product) => (
+              <div
+                key={product._id}
+                className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3"
+              >
+                <div className="bg-white p-4 rounded-lg shadow w-full h-full">
+                  <Product product={product} />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </>
   );
 };
