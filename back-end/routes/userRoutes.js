a5c14c951f00cbe0c8245033c225bddf725c5963
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-03-08 11:20:01 +0530

    Auth Middleware & Endpoint
    First of all we install the cookie parser because the allow easy to pass the cookie from the request object. then create the authMiddleware and create two function called protect and admin. then our route '/profile' protect the route and '/admin' route protect and admin.

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', userController.authUser);
 
 // router.get('/', userController.getUser);
 // router.post('/', userController.registerUser);
-router.route('/').get(userController.getUser).post(userController.registerUser);
+router
+  .route('/')
+  .get(protect, admin, userController.getUser)
+  .post(userController.registerUser);
 
 router.post('/logout', userController.logoutUser);
 
@@ -14,16 +18,16 @@ router.post('/logout', userController.logoutUser);
 // router.put('/profile' , userController.updateUserProfile);
 router
   .route('/profile')
-  .get(userController.getUserProfile)
-  .put(userController.updateUserProfile);
+  .get(protect, userController.getUserProfile)
+  .put(protect, userController.updateUserProfile);
 
 // router.delete('/:id', userController.deleteUser);
 // router.get('/:id', userController.getUserByID);
 // router.put('/:id', userController.updateUser);
 router
   .route('/:id')
-  .delete(userController.deleteUser)
-  .get(userController.getUserByID)
-  .put(userController.updateUser);
+  .delete(protect, admin, userController.deleteUser)
+  .get(protect, admin, userController.getUserByID)
+  .put(protect, admin, userController.updateUser);
 
 export default router;
--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -9,8 +10,13 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+// BOdy parser middleware / Allow to access req.body
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Cookie parser middleware / Allow to access req.cookie.jwt
+app.use(cookieParser());
 
 connectDB();
 
